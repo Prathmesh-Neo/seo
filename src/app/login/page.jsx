f9cf0bb9345eb980c8811c46985ab4b722897263
Prathmesh-Neo <prathmesh.gatade@neosoftmail.com>
2025-07-24 15:55:53 +0530

    First

---
+++ src/app/login/page.jsx
@@ -0,0 +1,21 @@
+export const metadata = {
+  title: 'login',
+  description: 'this is login page for user',
+  authors: [{ name: 'Prathmesh', url: 'https://prathmeshgatade-portfolio.vercel.app/' },
+  { name: 'Gatade' }],
+  keywords: ['nextjs', 'login', 'reactjs']
+}
+
+import React from 'react'
+
+function page() {
+  return (
+    <>
+      <div>
+        <h1>This is Login Page</h1>
+      </div>
+    </>
+  )
+}
+
+export default page
---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+
+import Head from "next/head";
+export default function Home() {
+
+  return (
+    <>
+    <Head>
+      <title>
+        Home page
+      </title>
+    </Head>
+      <h1 id="demo"> This is home page</h1>
+
+    </>
+  );
+}
